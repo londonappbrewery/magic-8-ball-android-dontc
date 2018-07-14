901ec75e0f863e23cd5790cf6f28ffd7c2878117
Mihail Beregoi <beregoi.mihail@gmail.com>
2018-07-14 23:10:49 +0300

    Fix random number generator value.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,7 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -29,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Random randomNumberGenerator = new Random();
-                int number = randomNumberGenerator.nextInt(4);
+                int number = randomNumberGenerator.nextInt(5);
+                Log.d("Random Number", "Array element: " + number);
 
                 ballDisplay.setImageResource(ballArray[number]);
 
